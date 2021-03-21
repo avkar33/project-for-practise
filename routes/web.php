5d6a7beb9973cebcc5579934c79fab9dd96acdfe
avkar33 <kmi3342@protonmail.com>
2021-03-21 19:40:11 +0300

    practise with routes|controllers|views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dd(request());
     return view('welcome');
 });
+Route::get('user/{id}', [TestController::class, 'user']);
